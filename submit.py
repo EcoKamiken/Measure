34df39dd33b2f838ce5a13d88cffc850480d9e16
Kazuki Nakajima <nakajima.k0301@gmail.com>
2019-07-02 17:34:51 +0900

    対象のディレクトリが存在しないときは、ディレクトリを作成するように変更

--- submit.py
+++ submit.py
@@ -31,23 +31,25 @@ site_id = config.getint('info', 'id')
 device_id = config.getint('info', 'device_id')
 post_to = config.get('info', 'post_to')
 
-with paramiko.SSHClient() as ssh:
-    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    ssh.connect(host, port=port, username=user, password=passwd)
+if __name__ == '__main__':
+    with paramiko.SSHClient() as ssh:
+        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        ssh.connect(host, port=port, username=user, password=passwd)
+        ssh.exec_command('mkdir -p ' + post_to + '/' + str(site_id) + '/' + str(device_id))
 
-    filename = 'log.csv'
-    backup = filename + '_' + current_time + '.csv'
+        filename = 'log.csv'
+        backup = filename.split('.')[0] + '_' + current_time + '.csv'
 
-    original_file_path = current_dir + '/' + filename
-    backup_file_path = current_dir + '/logs/' + backup
-    shutil.move(original_file_path, backup_file_path)
+        original_file_path = current_dir + '/' + filename
+        backup_file_path = current_dir + '/logs/' + backup
+        shutil.move(original_file_path, backup_file_path)
 
-    if transfer_protocol == 'SFTP':
-        with ssh.open_sftp() as sftp:
-            sftp.put(backup_file_path, post_to + '/' + str(site_id) + '/' +
-                     str(device_id) + '/' + backup)
-    elif transfer_protocol == 'SCP':
-        with scp.SCPClient(ssh.get_transport()) as scp:
-            scp.put(backup_file_path, post_to + '/' + str(site_id) + '/' +
-                    str(device_id) + '/' + backup)
+        if transfer_protocol == 'SFTP':
+            with ssh.open_sftp() as sftp:
+                sftp.put(backup_file_path, post_to + '/' + str(site_id) + '/' +
+                         str(device_id) + '/' + backup)
+        elif transfer_protocol == 'SCP':
+            with scp.SCPClient(ssh.get_transport()) as scp:
+                scp.put(backup_file_path, post_to + '/' + str(site_id) + '/' +
+                        str(device_id) + '/' + backup)
 
