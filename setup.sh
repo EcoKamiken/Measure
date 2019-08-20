1cada4809e61f439420e41af4310146354e3cc1b
Kazuki Nakajima <nakajima.k0301@gmail.com>
2019-08-20 17:51:40 +0900

    update

--- setup.sh
+++ setup.sh
@@ -5,4 +5,4 @@ cp example_config.ini config.ini
 
 # 必要なライブラリをインストール
 git clone https://github.com/szazo/DHT11_Python.git
-apt-get install -y python3-paramiko python3-scp python3-rpi.gpio postfix i2c-tools
+pip3.7 install --user -r requirements.txt
