02da6325b8f8c6fc79cd6d8361ad96c3a9f5e85d
Nakajima Kazuki <nakajima.k0301@gmail.com>
2019-12-02 18:17:25 +0900

    update

--- setup.sh
+++ setup.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-# コンフィグファイルをコピー
-if [[ ! -f config.ini ]]; then
-  cp example_config.ini config.ini
-fi
-
 # 必要なライブラリをインストール
 if [[ ! -e DHT11_Python ]]; then
   git clone https://github.com/szazo/DHT11_Python.git
